9c592fe4e181efdcea3208aa59dc47ea2b958adf
harithsiddiq <hind1123.ih@gmail.com>
2026-06-17 11:39:53 +0300

    fix(admin): switch to full Edit page so Lessons relation manager is visible

---
+++ app/Filament/Resources/CourseResource/Pages/EditCourse.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Resources\CourseResource\Pages;
+
+use App\Filament\Resources\CourseResource;
+use App\Filament\Resources\CourseResource\RelationManagers\LessonsRelationManager;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditCourse extends EditRecord
+{
+    protected static string $resource = CourseResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+
+    public function getRelationManagers(): array
+    {
+        return [
+            LessonsRelationManager::class,
+        ];
+    }
+}
